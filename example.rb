5d49ebfdb1b0eff9f02a8503aec831e31f2e1897
Isak Wiström <isak.wistrom@gmail.com>
2012-04-11 14:39:46 +0200

    changed message and channel in example

--- example.rb
+++ example.rb
@@ -2,10 +2,10 @@ require 'hydna'
 
 begin
   # sending messages
-  Hydna.push("https://public.hydna.net/4000", "Hello")
+  Hydna.push("https://public.hydna.net/4001", "Hello World")
   
   # sending signals
-  Hydna.emit("https://public.hydna.net/4000", "Hello") 
+  Hydna.emit("https://public.hydna.net/4001", "Hello World") 
   
 rescue Exception => e
   puts e.message
